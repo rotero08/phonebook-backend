24df81ebe43720b373a12d6e652a3e8dfcff7600
rotero08 <roteroerazo2@gmail.com>
2023-11-05 21:43:17 -0500

    exercise 3.2

--- index.js
+++ index.js
@@ -30,6 +30,14 @@ app.get("/", (request, response) => {
   response.send("<h1>Hello World!</h1>");
 });
 
+app.get("/info", (request, response) => {
+  const timestamp = new Date();
+  response.send(
+    `<p>Phonebook has info for ${persons.length} people</p>
+     <p>${timestamp}</p>`
+  );
+});
+
 app.get("/api/persons", (request, response) => {
   response.json(persons);
 });
