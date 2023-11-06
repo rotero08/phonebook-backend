85b369ca8f469b7bad0cca03ef615e20153a06f9
rotero08 <roteroerazo2@gmail.com>
2023-11-05 22:11:55 -0500

    exercise 3.5 and 3.6

--- index.js
+++ index.js
@@ -38,6 +38,10 @@ app.get("/info", (request, response) => {
   );
 });
 
+app.get("/api/persons", (request, response) => {
+  response.json(persons);
+});
+
 app.get("/api/persons/:id", (request, response) => {
   const id = Number(request.params.id);
   const person = persons.find((person) => person.id === id);
@@ -53,6 +57,41 @@ app.delete("/api/persons/:id", (request, response) => {
   persons = persons.filter((person) => person.id !== id);
 
   response.status(204).end();
+});
+
+const generateId = () => {
+  const maxId = persons.length > 0 ? Math.max(...persons.map((n) => n.id)) : 0;
+  return maxId + 1;
+};
+
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
+
+  const isDuplicate = persons.some(
+    (person) => JSON.stringify(person.name) === JSON.stringify(body.name)
+  );
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: "name or number missing",
+    });
+  }
+
+  if (isDuplicate) {
+    return response.status(400).json({
+      error: "name must be unique",
+    });
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId(),
+  };
+
+  persons = persons.concat(person);
+
+  response.json(person);
 });
 
 const PORT = 3001;
