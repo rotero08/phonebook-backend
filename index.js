0a514cd452bac9166dc4924a868ca86c17c106d1
rotero08 <roteroerazo2@gmail.com>
2023-11-06 00:56:11 -0500

    env PORT added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 var morgan = require("morgan");
+
 const app = express();
 
 app.use(express.json());
@@ -105,6 +106,7 @@ app.post("/api/persons", (request, response) => {
   response.json(person);
 });
 
-const PORT = 3001;
-app.listen(PORT);
-console.log(`Server running on port ${PORT}`);
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
