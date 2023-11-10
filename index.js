27793f8b4bbac5bcfacb285ee5380c2d0a8160dd
rotero08 <roteroerazo2@gmail.com>
2023-11-09 21:38:14 -0500

    exercise 3.9

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 var morgan = require("morgan");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 morgan.token("body", (req, res) => {
